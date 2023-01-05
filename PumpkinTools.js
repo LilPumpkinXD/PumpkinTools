7db4fdce41165da8ce2eb1f3df8683324313ac0f
LilPumpkinXD <122061017+LilPumpkinXD@users.noreply.github.com>
2023-01-05 18:14:53 -0500

    Update PumpkinTools.js

--- PumpkinTools.js
+++ PumpkinTools.js
@@ -1,24 +1,19 @@
-var title = document.createElement("div")
-title.className = "vaportitle"
-title.innerText = "PumpkinTools"
-appid.appendChild(title)
+var vaporwindow = `new Nightmare({
+"title": "PumpkinTools",
+"type": "js",
+"js": "https://cdn.jsdelivr.net/gh/FogNetwork/Vapor/app.min.js",
+"width": 800,
+"height": 600,
+"theme": "dark"
+})`
 
-var items = document.createElement("center")
+async function injectnightmare() {
+var NightmareWindows = await fetch("https://cdn.jsdelivr.net/gh/FogNetwork/Nightmare-Windows/windows.min.js")
+var NightmareCode = await NightmareWindows.text()
+NightmareCode = NightmareCode.replaceAll("<br>", ";")
+var cdn1 = document.createElement("script")
+cdn1.innerHTML = NightmareCode  + "\n\n" + "eval(" + vaporwindow + ")"
+document.getElementsByTagName("html")[0].appendChild(cdn1)
+}
 
-var tab = document.createElement("button")
-tab.className = "vaportab"
-tab.innerText = "Tab Cloak"
-tab.setAttribute("onclick", 'new Nightmare({"title": "Tab Cloak","type": "js","js": "https://cdn.jsdelivr.net/gh/LilPumpkinXD/PumpkinTools/TabCloak.min.js","width": 400,"height": 300,"theme": "dark"})')
-items.appendChild(tab)
-
-var inspect = document.createElement("button")
-inspect.className = "vaportab"
-inspect.innerText = "Edit"
-inspect.setAttribute("onclick", "javascript:alert('Coming Soon!'))
-items.appendChild(inspect)
-
-appid.appendChild(items)
-
-var vaporstyle = document.createElement("style")
-vaporstyle.innerText = ".vaportitle {font-size: 60px; text-align: center; color: white; user-select: none; margin-top: 10px;}.vaportab{cursor: pointer;background: #303134;border-radius: 2.5px;height: 50px;text-align: center;line-height: 50px;color: white; border: none; font-size: 18px;user-select: none; margin: 5px;} .vaportab:hover {background: darkslategrey;}"
-document.getElementsByTagName("html")[0].appendChild(vaporstyle)
+injectnightmare()
